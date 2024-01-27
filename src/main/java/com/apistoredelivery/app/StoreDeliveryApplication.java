6dba7aac3244573ea3bd1dbe157e6c0c43cc69a6
Gui <guilherme.rddev@gmail.com>
2024-01-26 21:02:06 -0300

    first commit

---
+++ src/main/java/com/apistoredelivery/app/StoreDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.apistoredelivery.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apistoredelivery/app/StoreDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apistoredelivery.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
