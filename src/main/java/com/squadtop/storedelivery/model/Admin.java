5f1df015cea6ace1b3017fcfd62d262014d64340
guirodg <guilherme.rddev@gmail.com>
2024-02-13 13:48:54 -0300

    feat: Add Struct Models

--- src/main/java/com/squadtop/storedelivery/model/Admin.java
+++ src/main/java/com/squadtop/storedelivery/model/Admin.java
@@ -1,21 +1,18 @@
 package com.squadtop.storedelivery.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class Cliente {
+public class Admin {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String nome;
-  @OneToMany
-  private List<Endereco> endereco;
-  private String email;
-  private Long telefone;
 }
