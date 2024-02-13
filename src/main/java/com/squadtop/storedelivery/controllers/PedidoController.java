705a30ec362b4c2132c0e538e1868abe995b9bff
Yann D'Souza <yannssouza@outlook.com>
2024-02-13 16:48:21 -0300

    feat: cadastrar pedido

--- src/main/java/com/squadtop/storedelivery/controllers/PedidoController.java
+++ src/main/java/com/squadtop/storedelivery/controllers/PedidoController.java
@@ -1,4 +1,29 @@
 package com.squadtop.storedelivery.controllers;
 
+import com.squadtop.storedelivery.model.Pedido;
+import com.squadtop.storedelivery.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Pedido>> getAll() {
+        return ResponseEntity.ok(pedidoRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Pedido> criandoPedido(@RequestBody Pedido pedido) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(pedidoRepository.save(pedido));
+    }
 }
